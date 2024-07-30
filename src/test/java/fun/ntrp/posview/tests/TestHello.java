92dce58808738af8167be63edc1ce8d2b10f242c
interrrp <int@addictedto.coffee>
2024-07-30 21:14:27 +0800

    test: set up JUnit 5 for testing

---
+++ src/test/java/fun/ntrp/posview/tests/TestHello.java
@@ -0,0 +1,12 @@
+package fun.ntrp.posview.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestHello {
+    @Test
+    public void testAdd() {
+        assertEquals(2 + 2, 4);
+    }
+}
