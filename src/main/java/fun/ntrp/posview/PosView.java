9bd88d380c0c5bb9e2dc02c72093ef90fbd7873e
interrrp <int@addictedto.coffee>
2024-07-30 21:04:29 +0800

    feat: show coordinates

--- src/main/java/fun/ntrp/posview/PosView.java
+++ src/main/java/fun/ntrp/posview/PosView.java
@@ -1,22 +1,39 @@
 package fun.ntrp.posview;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Colors;
+import net.minecraft.util.math.Vec3d;
 
 public class PosView implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("posview");
+	private static final int TEXT_POS_X = 16;
+	private static final int TEXT_POS_Y = 16;
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		HudRenderCallback.EVENT.register((context, deltaTickManager) -> {
+			draw(context);
+		});
+	}
 
-		LOGGER.info("Hello Fabric world!");
+	private static void draw(DrawContext context) {
+		MinecraftClient minecraft = MinecraftClient.getInstance();
+		PlayerEntity player = minecraft.player;
+		TextRenderer textRenderer = minecraft.textRenderer;
+
+		context.drawText(
+				textRenderer,
+				stringifyPos(player.getPos()),
+				TEXT_POS_X, TEXT_POS_Y,
+				Colors.WHITE,
+				/* Shadow */ true);
+	}
+
+	private static String stringifyPos(Vec3d pos) {
+		return String.format("%.1f %.1f %.1f", pos.x, pos.y, pos.z);
 	}
 }
