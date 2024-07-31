f19d7dfd1a446246facba623339f72e783925f72
interrrp <int@addictedto.coffee>
2024-07-31 15:01:05 +0800

    refactor: configuration screen

--- src/main/java/fun/ntrp/posview/ModMenuIntegration.java
+++ src/main/java/fun/ntrp/posview/ModMenuIntegration.java
@@ -18,51 +18,58 @@ public class ModMenuIntegration implements ModMenuApi {
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         Configuration cfg = Configuration.getInstance();
 
-        // TODO: Refactor this mess
+        Option<Integer> xOption = Option.<Integer>createBuilder()
+                .name(Text.literal("X"))
+                .binding(
+                        16,
+                        () -> cfg.getInt("x"),
+                        x -> cfg.setInt("x", x))
+                .controller(
+                        opt -> IntegerSliderControllerBuilder.create(opt)
+                                .range(0, 100)
+                                .step(1))
+                .build();
+
+        Option<Integer> yOption = Option.<Integer>createBuilder()
+                .name(Text.literal("Y"))
+                .binding(
+                        16,
+                        () -> cfg.getInt("y"),
+                        y -> cfg.setInt("y", y))
+                .controller(
+                        opt -> IntegerSliderControllerBuilder.create(opt)
+                                .range(0, 100)
+                                .step(1))
+                .build();
+
+        OptionGroup positionGroup = OptionGroup.createBuilder()
+                .name(Text.literal("Position"))
+                .option(xOption)
+                .option(yOption)
+                .build();
+
+        Option<Color> colorOption = Option.<Color>createBuilder()
+                .name(Text.literal("Color"))
+                .binding(Color.WHITE,
+                        () -> new Color(cfg.getInt("color")),
+                        color -> cfg.setInt("color", color.getRGB()))
+                .controller(ColorControllerBuilder::create)
+                .build();
+
+        OptionGroup appearanceGroup = OptionGroup.createBuilder()
+                .name(Text.literal("Appearance"))
+                .option(colorOption)
+                .build();
+
+        ConfigCategory mainCategory = ConfigCategory.createBuilder()
+                .name(Text.literal("Main settings"))
+                .group(positionGroup)
+                .group(appearanceGroup)
+                .build();
+
         return parentScreen -> YetAnotherConfigLib.createBuilder()
                 .title(Text.literal("Configure PosView"))
-                .category(ConfigCategory.createBuilder()
-                        .name(Text.literal("Main settings"))
-                        .group(OptionGroup.createBuilder()
-
-                                .name(Text.literal("Position"))
-
-                                .option(Option.<Integer>createBuilder()
-                                        .name(Text.literal("X"))
-                                        .binding(
-                                                16,
-                                                () -> cfg.getInt("x"),
-                                                x -> cfg.setInt("x", x))
-                                        .controller(
-                                                opt -> IntegerSliderControllerBuilder.create(opt)
-                                                        .range(0, 100)
-                                                        .step(1))
-                                        .build())
-
-                                .option(Option.<Integer>createBuilder()
-                                        .name(Text.literal("Y"))
-                                        .binding(
-                                                16,
-                                                () -> cfg.getInt("y"),
-                                                y -> cfg.setInt("y", y))
-                                        .controller(
-                                                opt -> IntegerSliderControllerBuilder.create(opt)
-                                                        .range(0, 100)
-                                                        .step(1))
-                                        .build())
-                                .build())
-
-                        .group(OptionGroup.createBuilder()
-                                .name(Text.literal("Appearance"))
-                                .option(Option.<Color>createBuilder()
-                                        .name(Text.literal("Color"))
-                                        .binding(Color.WHITE,
-                                                () -> new Color(cfg.getInt("color")),
-                                                color -> cfg.setInt("color", color.getRGB()))
-                                        .controller(ColorControllerBuilder::create)
-                                        .build())
-                                .build())
-                        .build())
+                .category(mainCategory)
                 .build()
                 .generateScreen(parentScreen);
     }
